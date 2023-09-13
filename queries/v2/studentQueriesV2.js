3e07944bcf4154b1ecfb579a746f13dc0c4114c2
ziqian pan <ziqianpan1998@gmail.com>
2023-09-13 19:56:25 -0400

    serve data from database, not json for v2

--- queries/v2/studentQueriesV2.js
+++ queries/v2/studentQueriesV2.js
@@ -1,20 +1,20 @@
-const studentsDataV2 = require("../../data/v2/studentsDataV2.json");
+const db = require("../../db");
 
 const { GetGradesByStudentIdV2 } = require("./gradesQueriesV2");
 
 
-const getAllStudentsV2 = () => {
-  const { students } = studentsDataV2;
+const getAllStudentsV2 = async () => {
+  const students = db.any('SELECT * FROM students');
   return students;
 };
 
-const getAllStudentsWithGradesV2 = () => {
+const getAllStudentsWithGradesV2 = async () => {
   const results = [];
 
-  const students = getAllStudentsV2();
+  const students = await getAllStudentsV2();
   for (const student of students) {
     const { id } = student;
-    const grades = GetGradesByStudentIdV2(id);
+    const grades = await GetGradesByStudentIdV2(id);
 
     const copy = { ...student };
     copy.grades = grades;
@@ -23,9 +23,8 @@ const getAllStudentsWithGradesV2 = () => {
   return results;
 };
 
-const getStudentByIdV2 = (id) => {
-  const { students } = studentsDataV2;
-  const student = students.find((student) => student.id === id);
+const getStudentByIdV2 = async (id) => {
+  const student = await db.oneOrNone('SELECT * FROM students where id = $1', [id]);
   return student;
 };
 
